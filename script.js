80bba8a15c01188fec9b9b6b65259765484b8e6a
faishal <faishalfarras.bts@gmail.com>
2023-10-31 15:15:01 +0700

    add search function

--- script.js
+++ script.js
@@ -2,20 +2,19 @@ const books = [];
 const RENDER_EVENT = "render-book";
 const SAVED_EVENT = "saved-book";
 const STORAGE_KEY = "BOOK_APPS";
+const ongoingReadList = document.getElementById("readOngoingList");
+const completedReadList = document.getElementById("readCompleteList");
 
 function isStorageExist() /* boolean */ {
-  if (typeof (Storage) === undefined) {
-    alert('Browser kamu tidak mendukung local storage');
+  if (typeof Storage === undefined) {
+    alert("Browser kamu tidak mendukung local storage");
     return false;
   }
   return true;
 }
 
 document.addEventListener(RENDER_EVENT, function () {
-  const ongoingReadList = document.getElementById("readOngoingList");
   ongoingReadList.innerHTML = "";
-
-  const completedReadList = document.getElementById("readCompleteList");
   completedReadList.innerHTML = "";
 
   for (const bookItem of books) {
@@ -30,11 +29,18 @@ document.addEventListener(RENDER_EVENT, function () {
 
 document.addEventListener("DOMContentLoaded", function () {
   const submitForm = document.getElementById("inputBook");
-  const searchForm = document
+  const searchForm = document.getElementById("searchBook");
+
   submitForm.addEventListener("submit", function (event) {
     event.preventDefault();
     addBook();
   });
+
+  searchForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    searchBook();
+  });
+
   if (isStorageExist()) {
     loadDataFromStorage();
   }
@@ -58,10 +64,31 @@ function changeSubmitButtonText() {
   const isCompleted = document.getElementById("inputBookIsComplete").checked;
 
   if (isCompleted) {
-    submitText.innerText = "Selesai dibaca"
+    submitText.innerText = "Selesai dibaca";
   } else {
-    submitText.innerText = "Belum selesai dibaca"
+    submitText.innerText = "Belum selesai dibaca";
   }
+}
+
+function searchBook() {
+  const searchValue = document.getElementById("searchBookTitle").value;
+  ongoingReadList.innerHTML = "";
+  completedReadList.innerHTML = "";
+
+  if (searchValue) {
+    const results = books.filter((book) =>
+      book.bookTitle.toLowerCase().includes(searchValue.toLowerCase())
+    );
+
+    for (const result of results) {
+      const bookResult = makeBookItem(result);
+      if (!result.isCompleted) {
+        ongoingReadList.append(bookResult);
+      } else {
+        completedReadList.append(bookResult);
+      }
+    }
+  } else document.dispatchEvent(new Event(RENDER_EVENT));
 }
 
 function addBook() {
